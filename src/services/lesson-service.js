969b8a647f54b8a7aaf60b448a6a53736d0ab0b3
Harrison Wong <harrisonjwong@yahoo.com>
2021-03-07 20:13:06 -0500

    Create lesson service

---
+++ src/services/lesson-service.js
@@ -0,0 +1,43 @@
+const MODULES_URL = 'https://wbdv-generic-server.herokuapp.com/api/hwong/modules';
+const LESSONS_URL = 'https://wbdv-generic-server.herokuapp.com/api/hwong/lessons';
+
+const createLesson = (moduleId, lesson) =>
+  fetch(`${MODULES_URL}/${moduleId}/lessons`, {
+    method: 'POST',
+    body: JSON.stringify(lesson),
+    headers: {
+      'content-type': 'application/json'
+    }
+  }).then(response => response.json());
+
+const findLessonsForModule = (moduleId) =>
+  fetch(`${MODULES_URL}/${moduleId}/lessons`)
+    .then(response => response.json());
+
+const findLesson = (lessonId) =>
+  fetch(`${LESSONS_URL}/${lessonId}`)
+    .then(response => response.json());
+
+const updateLesson = (lessonId, lesson) =>
+  fetch(`${LESSONS_URL}/${lessonId}`, {
+    method: 'PUT',
+    body: JSON.stringify(lesson),
+    headers: {
+      'content-type': 'application/json'
+    }
+  }).then(response => response.json());
+
+const deleteLesson = (lessonId) =>
+  fetch(`${LESSONS_URL}/${lessonId}`, {
+    method: 'DELETE'
+  }).then(response => response.json());
+
+const api = {
+  createLesson,
+  findLessonsForModule,
+  findLesson,
+  updateLesson,
+  deleteLesson
+};
+
+export default api;
