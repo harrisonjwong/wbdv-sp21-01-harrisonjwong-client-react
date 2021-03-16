05d03a95b2d58fcaa488a71004e04c65b4f4b457
Harrison Wong <harrisonjwong@yahoo.com>
2021-03-16 00:29:28 -0400

    Create paragraph widget

---
+++ src/components/widgets/paragraph-widget.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ParagraphWidget = ({widget, editing, setEditing}) => {
+  const onChangeTextarea = (text) => {
+    const withoutCurrWidget = editing.filter(w => w.id !== widget.id);
+    widget.text = text;
+    setEditing([...withoutCurrWidget, widget])
+  }
+
+  return (
+    <div>
+      {
+        !editing.includes(widget) && <p>{widget.text}</p>
+      }
+      {
+        editing.includes(widget) &&
+        <textarea
+          onChange={e => onChangeTextarea(e.target.value)}
+          value={widget.text}
+          className='form-control'
+          rows={5}/>
+      }
+    </div>
+  );
+}
+
+export default ParagraphWidget;
