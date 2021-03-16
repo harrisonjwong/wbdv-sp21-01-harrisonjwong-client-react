72a68f3adf3ee890d134b879f3b6f8b13c40b57c
Harrison Wong <harrisonjwong@yahoo.com>
2021-03-16 00:28:26 -0400

    Update course editor: add widget reducer and widget list component

--- src/components/course-editor/course-editor.js
+++ src/components/course-editor/course-editor.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import moduleReducer from '../../reducers/module-reducer';
 import lessonReducer from '../../reducers/lesson-reducer';
 import topicReducer from '../../reducers/topic-reducer';
+import widgetReducer from '../../reducers/widget-reducer';
 import {combineReducers, createStore} from 'redux';
 import {Link, useParams} from 'react-router-dom';
 import {Provider} from 'react-redux';
@@ -9,17 +10,19 @@ import ModuleList from './module-list';
 import LessonTabs from './lesson-tabs';
 import TopicPills from './topic-pills';
 import courseService from '../../services/course-service';
+import WidgetList from '../widgets/widget-list';
 
 const reducer = combineReducers({
   moduleReducer,
   lessonReducer,
-  topicReducer
+  topicReducer,
+  widgetReducer
 });
 
 const store = createStore(reducer);
 
 const CourseEditor = () => {
-  const { layout, courseId, moduleId, lessonId } = useParams();
+  const { layout, courseId, moduleId, lessonId, topicId } = useParams();
   const [courseName, setCourseName] = useState('')
   useEffect(() => {
     courseService.findCourseById(courseId).then((res) => {
@@ -28,7 +31,7 @@ const CourseEditor = () => {
   }, [courseId])
   return (
     <Provider store={store}>
-      <>
+      <div className='wbdv-editor-bottom-padding'>
         <h2>
           <Link to={`/courses/${layout}`}>
             <i className='fas fa-times wbdv-close-editor-button'/>
@@ -47,9 +50,13 @@ const CourseEditor = () => {
             {
               lessonId && <TopicPills/>
             }
+            <br/>
+            {
+              topicId && <WidgetList/>
+            }
           </div>
         </div>
-      </>
+      </div>
     </Provider>
   );
 }
