3a3401c60d647a1e843b8e011065581d8b0ffb77
Harrison Wong <harrisonjwong@yahoo.com>
2021-04-04 16:19:36 -0400

    Create quiz-service

---
+++ src/services/quiz-service.js
@@ -0,0 +1,15 @@
+const QUIZZES_URL = 'http://localhost:3001/api/quizzes';
+
+export const findAllQuizzes = () =>
+  fetch(QUIZZES_URL)
+    .then(response => response.json());
+
+export const findQuizById = (qid) =>
+  fetch(`${QUIZZES_URL}/${qid}`)
+    .then(response => response.json())
+
+const api = {
+  findAllQuizzes, findQuizById
+}
+
+export default api;
