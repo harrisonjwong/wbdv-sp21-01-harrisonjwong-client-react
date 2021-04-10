dfc8bff838dcda717a32a4814f2baef962af6b69
Harrison Wong <harrisonjwong@yahoo.com>
2021-04-10 16:01:01 -0400

    update question

--- src/components/quizzes/question.js
+++ src/components/quizzes/question.js
@@ -2,16 +2,22 @@ import React from 'react';
 import TrueFalseQuestion from './true-false-question';
 import MultipleChoiceQuestion from './multiple-choice-question';
 
-const Question = ({question}) => {
+const Question = ({question, questions, setQuestions, graded}) => {
   return (
     <div>
       {
         question.type === 'TRUE_FALSE' &&
-          <TrueFalseQuestion question={question}/>
+          <TrueFalseQuestion question={question}
+                             questions={questions}
+                             setQuestions={setQuestions}
+                             graded={graded}/>
       }
       {
         question.type === 'MULTIPLE_CHOICE' &&
-          <MultipleChoiceQuestion question={question}/>
+          <MultipleChoiceQuestion question={question}
+                                  questions={questions}
+                                  setQuestions={setQuestions}
+                                  graded={graded}/>
       }
     </div>
   );
