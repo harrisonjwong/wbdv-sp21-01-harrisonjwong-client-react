f803a16962ce332af3fa43b7fd85dfbd612ca4a6
Harrison Wong <harrisonjwong@yahoo.com>
2021-04-16 15:52:15 -0400

    switch questions and widgets to remote (Heroku) URLs

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,4 +1,5 @@
-const QUIZZES_URL = 'http://localhost:3001/api/quizzes';
+// const QUIZZES_URL = 'http://localhost:3001/api/quizzes';
+const QUIZZES_URL = 'https://wbdv-sp21-harrisonjwong-node.herokuapp.com/api/quizzes';
 
 export const findQuestionsForQuiz = (qid) =>
   fetch(`${QUIZZES_URL}/${qid}/questions`)
--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -1,4 +1,5 @@
-const QUIZZES_URL = 'http://localhost:3001/api/quizzes';
+// const QUIZZES_URL = 'http://localhost:3001/api/quizzes';
+const QUIZZES_URL = 'https://wbdv-sp21-harrisonjwong-node.herokuapp.com/api/quizzes';
 
 export const findAllQuizzes = () =>
   fetch(QUIZZES_URL)
